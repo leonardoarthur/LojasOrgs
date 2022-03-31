603a27550374468ef3ca8c1fa7073edacc547f48
Leonardo Arthur <leonardo.aguiar@foursys.com.br>
2022-03-31 16:33:16 -0300

    lojas Orgânicas

---
+++ src/mocks/topo.js
@@ -0,0 +1,6 @@
+const topo = {
+    boasVindas: "Olá Leonardo",
+    legenda: "Encontre os melhores prosutores",
+};
+
+export default topo;
---
+++ src/telas/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Produtores from "./componentes/Produtores";
+import Topo from './componentes/Topo';
+
+export default function Home() {
+    return <Produtores topo={Topo} />
+}
